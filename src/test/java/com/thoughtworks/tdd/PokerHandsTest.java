fa37d0b35cb687b81607e5e71128b83e165ddb71
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-08-11 10:41:22 +0800

    initial upload

---
+++ src/test/java/com/thoughtworks/tdd/PokerHandsTest.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PokerHandsTest {
+    @Test
+    public void initialTest() {
+        System.out.println("hello world");
+    }
+}
